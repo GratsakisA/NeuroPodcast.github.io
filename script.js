dcdd5252f3422bdb4a20b8700f309e6499c36330
Anastasios Gratsakis <143262163+GratsakisA@users.noreply.github.com>
2025-04-27 17:46:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,17 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+// Highlight the active page link in gray
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split("/").pop();
+    const navLinks = document.querySelectorAll(".nav-link");
+  
+    navLinks.forEach(link => {
+      const linkPage = link.getAttribute("href");
+      if (linkPage === currentPage) {
+        link.classList.add("active-page");
+      }
+    });
+  });
+  
